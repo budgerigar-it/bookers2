880422caa0cb67301a8d17c308e5f6d7cdc91f6d
budgerigar-it <230291598+budgerigar-it@users.noreply.github.com>
2025-10-04 17:09:56 +0900

    [Finish] bookers2-favorite

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   resources :books do
     resources :book_comments, only: [:create, :destroy]
+    resource :favorite, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
